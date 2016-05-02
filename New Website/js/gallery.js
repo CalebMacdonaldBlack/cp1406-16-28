0f5a8e727b522b5f29a6632a9ecc632b3f7c68ec
emersonbaria <emerson.baria@my.jcu.edu.au>
2016-05-02 13:34:30 +1000

    people and gallery pages for approval

--- New Website/js/gallery.js
+++ New Website/js/gallery.js
@@ -15,42 +15,59 @@ function rotate(x) {
 }
 
 function group_select(x){
+    var slavonicswoon = document.getElementById('gallery_thumbnails_slavonicswoon');
+    var hotsummer = document.getElementById('gallery_thumbnails_hotsummer');
+    var lastnight = document.getElementById('gallery_thumbnails_lastnight');
     if (x==1) {
-        document.getElementById('gallery_thumbnails_slavonicswoon').style.display = ('block');
-        document.getElementById('gallery_thumbnails').style.display = ('none');
-        document.getElementById('gallery_thumbnails_lastnight').style.display = ('none');
+        slavonicswoon.style.display = ('block');
+        hotsummer.style.display = ('none');
+        lastnight.style.display = ('none');
         document.getElementById('img').src = 'images/slavonicswoon/img1.jpg';
-        document.getElementById('label').innerHTML =('Slavonic Swoon 2008');
+        document.getElementById('label').innerHTML =('SLAVONIC SWOON 2008');
         numofimages = 9;
-        group = 'Images/slavonicswoon/img'
-        document.getElementById(p3).style.opacity=1;
-        document.getElementById(p2).style.opacity = .5;
-        document.getElementById(p1).style.opacity = .5;
+        group = 'Images/slavonicswoon/img';
+        document.getElementById('p1').style.opacity = 1;
+        document.getElementById('p2').style.opacity = .5;
+        document.getElementById('p3').style.opacity = .5;
 
     }
-    else if (x=2){
-        document.getElementById('gallery_thumbnails_slavonicswoon').style.display = ('none');
-        document.getElementById('gallery_thumbnails').style.display = ('block');
-        document.getElementById('gallery_thumbnails_lastnight').style.display = ('none');
+    else if (x==2){
+        slavonicswoon.style.display = ('none');
+        hotsummer.style.display = ('block');
+        lastnight.style.display = ('none');
         document.getElementById('img').src = 'images/hotsummernight2008/img1.jpg';
-        document.getElementById('label').innerHTML =('Hot Summer Night 2008');
+        document.getElementById('label').innerHTML =('HOT SUMMER NIGHT 2008');
         numofimages = 32;
-        group = 'Images/hotsummernight2008/img'
-        document.getElementById(p3).style.opacity = .5;
-        document.getElementById(p2).style.opacity = .5;
-        document.getElementById(p1).style.opacity = 1;
+        group = 'Images/hotsummernight2008/img';
+        document.getElementById('p1').style.opacity = .5;
+        document.getElementById('p2').style.opacity = 1;
+        document.getElementById('p3').style.opacity = .5;
     }
     else {
-        document.getElementById('gallery_thumbnails_slanovicswoon').style.display = ('none');
-        document.getElementById('gallery_thumbnails').style.display = ('none');
-        document.getElementById('gallery_thumbnails_lastnight').style.display = ('block');
+        slavonicswoon.style.display = ('none');
+        hotsummer.style.display = ('none');
+        lastnight.style.display = ('block');
         document.getElementById('img').src = 'images/lastnightofproms/img1.jpg';
-        document.getElementById('label').innerHTML =('Last Night of the Proms 2009');
+        document.getElementById('label').innerHTML =('LAST NIGHT OF THE PROMS 2009');
         numofimages = 27;
-        group = 'Images/lastnightofproms/img'
-        document.getElementById(p3).style.background = 'red';
-        document.getElementById(p2).style.opacity = 1;
-        document.getElementById(p1).style.opacity = .5;
+        group = 'Images/lastnightofproms/img';
+        document.getElementById('p1').style.opacity = .5;
+        document.getElementById('p2').style.opacity = .5;
+        document.getElementById('p3').style.opacity = 1;
+    }
+}
+function select(y,z) {
+    var a = 'images/slavonicswoon/img';
+    var b = 'images/hotsummernight2008/img';
+    var c = 'images/lastnightofproms/img';
+    var Image = document.getElementById('img');
+
+    if (y==1) {
+        Image.src=a+z+'.jpg';
+    } else if(y==2){
+        Image.src=b+z+'.jpg';
+    } else {
+        Image.src=c+z+'.jpg';
     }
 }
 
